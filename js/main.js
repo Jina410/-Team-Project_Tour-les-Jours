43034b4a5047397b135269ce72cd3157c147efb8
jina410 <rhdqnwlsk@gmail.com>
2025-10-01 14:35:54 +0900

    251001.jina2

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(function () {
+
+  //SWIPER 배너 슬라이드
+  var swiper = new Swiper(".main-mySwiper1", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+      formatFractionCurrent: function (number) {
+        return number = '0' + number;
+      },
+      formatFractionTotal: function (number) {
+        return number = '0' + number;
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    loop: true,
+  });
+
+  //SWIPER 추천제품 이벤트 슬라이드
+  var swiper = new Swiper(".main-mySwiper2", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    loop: true,
+  });
+
+  //SWIPER 추천제품 제품 슬라이드
+  var swiper = new Swiper(".main-mySwiper3", {
+    slidesPerView: 3,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    loop: true,
+  });
+})
+
