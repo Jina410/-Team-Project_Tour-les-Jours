c15f6d7ffbb6262e19a2b98d8db0bd6b0d009dcf
jina410 <rhdqnwlsk@gmail.com>
2025-10-02 11:08:45 +0900

    251002.jina4

--- js/common.js
+++ js/common.js
@@ -10,3 +10,17 @@ $(function () {
     $(".menu-bg, .sub-menu").stop().slideUp(200);
   })
 })
+
+
+$(function(){
+  $(window).scroll(function(){
+    let st = $(window).scrollTop();
+    let winH = $(window).height();
+
+    if( st > winH - 300){
+      $(".top").addClass("on");
+    } else{
+      $(".top").removeClass("on");
+    }
+  })
+})
