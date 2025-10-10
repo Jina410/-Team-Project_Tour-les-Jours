77fcff51041dfb535c612640e4edb9a718d3ce7d
jina410 <rhdqnwlsk@gmail.com>
2025-10-10 15:48:07 +0900

    251010.jina2

--- js/main.js
+++ js/main.js
@@ -14,28 +14,27 @@ $(function () {
 })
 
 $(function () {
-  //SWIPER 배너 슬라이드
-  var swiper = new Swiper(".main-mySwiper1", {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-      formatFractionCurrent: function (number) {
-        return number = '0' + number;
-      },
-      formatFractionTotal: function (number) {
-        return number = '0' + number;
-      },
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-      delay: 8000,
-      disableOnInteraction: false
-    },
-    loop: true,
-  });
+//   var swiper = new Swiper(".main-mySwiper1", {
+//     pagination: {
+//       el: ".swiper-pagination",
+//       type: "fraction",
+//       formatFractionCurrent: function (number) {
+//         return number = '0' + number;
+//       },
+//       formatFractionTotal: function (number) {
+//         return number = '0' + number;
+//       },
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     autoplay: {
+//       delay: 8000,
+//       disableOnInteraction: false
+//     },
+//     loop: true,
+//   });
 
   //SWIPER 추천제품 이벤트 슬라이드
   var swiper = new Swiper(".main-mySwiper2", {
